feaef4ee384f5c036f7e2bc737df50501651a4f2
pyanodon <smylodon@gmail.com>
2018-01-14 12:04:56 -0200

    finished-red circuit

--- prototypes/updates/bob-updates.lua
+++ prototypes/updates/bob-updates.lua
@@ -1,5 +1,9 @@
 Recipe("electronic-circuit"):remove_unlock("electronics")
 Recipe("basic-electronic-components"):remove_unlock("electronics")
-Recipe("phenolic-board"):remove_unlock("electronics")
+Recipe("phenolic-board"):remove_unlock("advanced-electronics")
+Recipe("circuit-board"):remove_unlock("advanced-electronics")
+Recipe("advanced-circuit"):remove_unlock("advanced-electronics")
 
---change_value("wooden-board", "category", nil)
+
+
+--change_value("wooden-board", "category", "erase")
