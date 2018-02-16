0ee528f68ce764d9a1a9dd471a585f0573f7e157
pyanodon <smylodon@gmail.com>
2018-02-16 10:44:42 -0200

    nano-stuff
    Former-commit-id: 4fc9432a28ced78613c1eff4d28121be52e776d5
    Former-commit-id: db0a3cda22c410205ebab166b833f1c16922b73a

---
+++ prototypes/fluids/bacteria-2.lua
@@ -0,0 +1,32 @@
+Recipe {
+    type = "recipe",
+    name = "bacteria-2",
+    category = "genlab",
+    enabled = false,
+    energy_required = 8,
+    ingredients = {
+        {type = "item", name = "bonemeal", amount = 5},
+		{type = "fluid", name = "oleochemicals", amount = 100},
+        {type = "item", name = "ground-sample01", amount = 8},
+    },
+    results = {
+        {type = "fluid", name = "bacteria-2", amount = 50}
+    },
+    main_product = "bacteria-2",
+}:add_unlock("nano-tech")
+
+Fluid {
+    type = "fluid",
+    name = "bacteria-2",
+    icon = "__pyhightech__/graphics/icons/bacteria-2.png",
+	icon_size = 32,
+    default_temperature = 15,
+    base_color = {r = 0.478, g = 0.8, b = 0.784},
+    flow_color = {r = 0.478, g = 0.8, b = 0.784},
+    max_temperature = 100,
+    gas_temperature = 15,
+    pressure_to_speed_ratio = 0.4,
+    flow_to_energy_ratio = 0.59,
+    subgroup = "py-hightech-fluids",
+    order = "c"
+}
