5db19052a338b50a3540f57ff213e18ba13eb1cb
pyanodon <smylodon@gmail.com>
2018-03-28 23:34:11 -0300

    balances-pre-release
    Former-commit-id: b8edd255033e30e2947763fb24be8f5aae17ca57
    Former-commit-id: 9295356ce8174ef3cb31e5a0d345910137031085

--- prototypes/items/items.lua
+++ prototypes/items/items.lua
@@ -1450,3 +1450,13 @@ ITEM {
     order = "h",
     stack_size = 200
 }
+ITEM {
+    type = "item",
+    name = "dried-meat",
+    icon = "__pyhightech__/graphics/icons/dried-meat.png",
+    icon_size = 32,
+    flags = {"goes-to-main-inventory"},
+    subgroup = "py-hightech-items",
+    order = "h",
+    stack_size = 200
+}
