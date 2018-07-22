0dfb06630799e4b11bf3ea04725daed6a5843d1d
pyanodon <smylodon@gmail.com>
2018-07-22 17:51:59 -0300

    added recipes

--- prototypes/fluids/ethylene.lua
+++ prototypes/fluids/ethylene.lua
@@ -1,29 +1,29 @@
 RECIPE {
     type = "recipe",
-    name = "propene",
+    name = "ethylene",
     category = "gasifier",
     enabled = false,
     energy_required = 6,
     ingredients = {
-        {type = "fluid", name = "syngas", amount = 60},
+        {type = "fluid", name = "methane", amount = 50},
         {type = "fluid", name = "steam", amount = 200, temperature = 165},
-        {type = "item", name = "iron-plate", amount = 5}
+        {type = "item", name = "iron-oxide", amount = 2}
     },
     results = {
-        {type = "fluid", name = "propene", amount = 60},
+        {type = "fluid", name = "ethylene", amount = 60},
         {type = "fluid", name = "water", amount = 50}
     },
-    main_product = "propene",
-}:add_unlock("basic-electronics"):replace_ingredient("iron-plate", "lead-plate")
+    main_product = "ethylene",
+}:add_unlock("basic-electronics"):replace_ingredient('steam', 'pressured-air')
 
 FLUID {
     type = "fluid",
-    name = "propene",
-    icon = "__pyhightech__/graphics/icons/propene.png",
+    name = "ethylene",
+    icon = "__pyhightech__/graphics/icons/ethylene.png",
 	icon_size = 32,
     default_temperature = 15,
-    base_color = {r = 0.572, g = 0.435, b = 0.666},
-    flow_color = {r = 0.572, g = 0.435, b = 0.666},
+    base_color = {r = 0.896, g = 0.805, b = 0.762},
+    flow_color = {r = 0.896, g = 0.805, b = 0.762},
     max_temperature = 100,
     gas_temperature = 15,
     pressure_to_speed_ratio = 0.4,
