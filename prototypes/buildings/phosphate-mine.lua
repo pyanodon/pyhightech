012fbf82c573a4ff5f3f76e6a58a02e7fe01d3ac
kingarthur91 <arthurkoy_durbin@yahoo.com>
2019-04-20 22:17:45 -0400

    update pollution values to the easier to use per minute

--- prototypes/buildings/phosphate-mine.lua
+++ prototypes/buildings/phosphate-mine.lua
@@ -65,7 +65,7 @@ ENTITY {
       fuel_category = "drill",
       effectivity = 3,
       fuel_inventory_size = 1,
-      emissions_per_second_per_watt = 1e-06,
+      emissions_per_minute = 50,
       smoke =
       {
         {
