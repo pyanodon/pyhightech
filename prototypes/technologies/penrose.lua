e05508a4a8dd634290187f607f0b5a79319e9841
pyanodon <smylodon@gmail.com>
2019-05-20 20:16:06 -0300

    added blackhole
    Former-commit-id: 7245a888fa6c6f2af1b11d06491433ab0f258525
    Former-commit-id: 6489cef5a123b07b9add74808c8a7b3353161cf6

---
+++ prototypes/technologies/penrose.lua
@@ -0,0 +1,18 @@
+TECHNOLOGY {
+    type = "technology",
+    name = "penrose",
+    icon = "__pyhightech__/graphics/technology/penrose.png",
+    icon_size = 128,
+    order = "c-a",
+    prerequisites = {"nano-tech","earnshaw-theorem"},
+    effects = {},
+    unit = {
+        count = 150,
+        ingredients = {
+            {"automation-science-pack", 3},
+            {"logistic-science-pack", 2},
+            {"chemical-science-pack", 1},
+        },
+        time = 55
+    }
+}
