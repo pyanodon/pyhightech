c25dece0d444d34e0ef03110226bd9e41e8cdc7d
kingarthur91 <arthurkoy_durbin@yahoo.com>
2019-05-22 13:13:59 -0400

    finish up the code for the blackhole generator
    Former-commit-id: f2018a6104c6c36bd23675e3fb643938120f3a77
    Former-commit-id: 1f8426c0693b5fc3b5570c79e4e6f156c660cb69

--- control.lua
+++ control.lua
@@ -30,7 +30,7 @@ local Furn = game.surfaces["nauvis"].create_entity{name="magic-furnace",position
 
 local newgen = util.table.deepcopy(global.blackhole[1])
 
-log(serpent.block(newgen))
+--log(serpent.block(newgen))
 
 newgen.counter=global.gencounter
 newgen.generator.name = E.name
@@ -49,17 +49,28 @@ end
 
 end)
 
-script.on_event(defines.events.on_tick,function(event)
-
---fuelcheck(event)
-
-if event.tick % 30 == 0 then
+script.on_nth_tick(30,function(fuelcheck)
 
 for _,e in pairs(global.blackhole) do
 
---log(serpent.block(e))
+	--log(serpent.block(e))
+	--log(serpent.block(e.generator.pos))
+	local curgen = game.surfaces["nauvis"].find_entity("blackhole",e.generator.pos)
+	local curfurn = game.surfaces["nauvis"].find_entity("magic-furnace",e.furnace.pos)
 
-end
+	if curgen ~= nil then
+		--log(serpent.block(curgen.name))
+		--log(serpent.block(curfurn.name))
+		if curgen.get_fuel_inventory().get_item_count() <= 5 then
+			--log("low fuel")
+			local fuel = curfurn.get_output_inventory().get_item_count()
+			if fuel >= 1 then
+				curgen.get_fuel_inventory().insert({name="blackhole-fuel",count=fuel})
+				
+				curfurn.get_output_inventory().clear()
+			end
+		end
+	end
 
 end
 
@@ -67,4 +78,20 @@ end)
 
 function fuelcheck(event)
 
+for _,e in pairs(global.blackhole) do
+
+log(serpent.block(e))
+
+
 end
+
+end
+
+script.on_event({defines.events.on_player_mined_entity, defines.events.on_robot_mined_entity}, function(event)
+	if event.entity.name == "blackhole" then
+		local f = game.surfaces["nauvis"].find_entity("magic-furnace",event.entity.position)
+		if f ~= nil then
+			f.destroy()
+		end
+	end
+end)
