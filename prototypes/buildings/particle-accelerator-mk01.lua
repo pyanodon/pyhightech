f212d8f585e2f5883209894a5681dddfcfcc818c
kingarthur91 <arthurkoy_durbin@yahoo.com>
2019-11-09 18:29:24 -0500

    added fix for modules in pa
    Former-commit-id: 62af71047f303245b563a580d1b9ef2d3ac24375
    Former-commit-id: 79e9f956b8052e9b8080275f3183a377527fe895

--- prototypes/buildings/particle-accelerator-mk01.lua
+++ prototypes/buildings/particle-accelerator-mk01.lua
@@ -44,7 +44,7 @@ ENTITY {
     module_specification = {
         module_slots = 1
     },
-    allowed_effects = {"speed"},
+    allowed_effects = {"consumption", "speed", "productivity", "pollution"},
     crafting_categories = {"pa"},
     crafting_speed = 1,
     energy_source = {
---
+++ prototypes/functions/effectivity-modules.lua
@@ -0,0 +1,31 @@
+local modules={}
+
+function modules.effectivityblacklist()
+
+	local effectlimitaionlist = {}
+	local set = {}
+
+	--convert recipe table into a set
+	for r, recipe in pairs(data.raw.recipe) do
+		if data.raw.recipe[recipe.name].category == 'pa' then
+			set[recipe.name] = true
+		end
+	end
+		
+	for r ,recipe in pairs(data.raw.recipe) do
+		if set[recipe.name] == nil then
+			table.insert(effectlimitaionlist, recipe.name)
+		end
+	end
+	--log(serpent.block(set))
+	--log(serpent.block(effectlimitaionlist))
+	data.raw.module["effectivity-module"].limitation = effectlimitaionlist
+	data.raw.module["effectivity-module"].limitation_message_key = ' lick my balls'
+	data.raw.module["effectivity-module-2"].limitation = effectlimitaionlist
+	data.raw.module["effectivity-module-2"].limitation_message_key = ' lick my balls'
+	data.raw.module["effectivity-module-3"].limitation = effectlimitaionlist
+	data.raw.module["effectivity-module-3"].limitation_message_key = ' lick my balls'
+
+end
+
+return modules
