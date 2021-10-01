740f4b5fa979b1d017b922d2564f7174aa768ccb
kingarthur91 <arthurkoy_durbin@yahoo.com>
2021-10-01 05:40:47 -0400

    steam temps

--- prototypes/recipes/recipes.lua
+++ prototypes/recipes/recipes.lua
@@ -131,7 +131,7 @@ RECIPE {
     results = {
         {type = 'item', name = 'rich-dust', amount = 5},
         {type = 'fluid', name = 'rare-earth-mud', amount = 250},
-        {type = 'fluid', name = 'steam', amount = 2000, temperature = 60},
+        {type = 'fluid', name = 'steam', amount = 2000, temperature = 150},
         {type = 'item', name = 'sand', amount = 10}
     },
     main_product = 'rare-earth-mud',
@@ -3095,7 +3095,7 @@ RECIPE {
     results = {
         {type = 'fluid', name = 'critical-steam', amount = 20000, temperature = 5000},
         {type = 'fluid', name = 'helium', amount = 500},
-        {type = 'fluid', name = 'steam', amount = 5000, temperature = 60}
+        {type = 'fluid', name = 'steam', amount = 5000, temperature = 150}
     },
     --main_product= "blanket",
     icon = '__pyhightechgraphics__/graphics/icons/fusion-antimatter.png',
@@ -3116,7 +3116,7 @@ RECIPE {
     },
     results = {
         {type = 'fluid', name = 'pressured-steam', amount = 1000, temperature = 5000},
-        {type = 'fluid', name = 'steam', amount = 400, temperature = 60}
+        {type = 'fluid', name = 'steam', amount = 400, temperature = 150}
     },
     --main_product= "blanket",
     icon = '__pyhightechgraphics__/graphics/icons/steam-exchange5.png',
@@ -3388,7 +3388,7 @@ RECIPE {
     },
     results = {
         {type = 'fluid', name = 'combustion-mixture1', amount = 150, temperature = 550},
-        {type = 'fluid', name = 'steam', amount = 500, temperature = 60}
+        {type = 'fluid', name = 'steam', amount = 500, temperature = 150}
     },
     icon = '__pyhightechgraphics__/graphics/icons/combustion-methane.png',
     icon_size = 32,
@@ -3445,7 +3445,7 @@ RECIPE {
     },
     results = {
         {type = 'fluid', name = 'combustion-mixture1', amount = 150, temperature = 520},
-        {type = 'fluid', name = 'steam', amount = 500, temperature = 60}
+        {type = 'fluid', name = 'steam', amount = 500, temperature = 150}
     },
     icon = '__pyhightechgraphics__/graphics/icons/combustion-benzene.png',
     icon_size = 32,
@@ -3502,7 +3502,7 @@ RECIPE {
     },
     results = {
         {type = 'fluid', name = 'combustion-mixture1', amount = 150, temperature = 600},
-        {type = 'fluid', name = 'steam', amount = 500, temperature = 60}
+        {type = 'fluid', name = 'steam', amount = 500, temperature = 150}
     },
     icon = '__pyhightechgraphics__/graphics/icons/combustion-tall-oil.png',
     icon_size = 32,
