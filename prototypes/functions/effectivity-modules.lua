e0fd31f899f5928cdcfaeb1ded0f0def21462e25
kingarthur91 <arthurkoy_durbin@yahoo.com>
2021-12-06 02:17:46 -0500

    rewrote effectivity list to add disallowed recipes to blacklist

--- prototypes/functions/effectivity-modules.lua
+++ prototypes/functions/effectivity-modules.lua
@@ -18,17 +18,21 @@ function modules.effectivityblacklist(cat)
 	end
 
 	for _,recipe in pairs(data.raw.recipe) do
-		if set[recipe.name] == nil then
-			table.insert(effectlimitaionlist, recipe.name)
+		if set[recipe.name] then
+			--table.insert(effectlimitaionlist, recipe.name)
+			table.insert(data.raw.module["effectivity-module"].limitation_blacklist, recipe.name)
+			table.insert(data.raw.module["effectivity-module-2"].limitation_blacklist, recipe.name)
+			table.insert(data.raw.module["effectivity-module-2"].limitation_blacklist, recipe.name)
 		end
 	end
 	--log(serpent.block(set))
 	--log(serpent.block(effectlimitaionlist))
-	data.raw.module["effectivity-module"].limitation = effectlimitaionlist
+
+	--data.raw.module["effectivity-module"].limitation_blacklist = effectlimitaionlist
 	data.raw.module["effectivity-module"].limitation_message_key = 'lick my balls'
-	data.raw.module["effectivity-module-2"].limitation = effectlimitaionlist
+	--data.raw.module["effectivity-module-2"].limitation_blacklist = effectlimitaionlist
 	data.raw.module["effectivity-module-2"].limitation_message_key = 'lick my balls'
-	data.raw.module["effectivity-module-3"].limitation = effectlimitaionlist
+	--data.raw.module["effectivity-module-3"].limitation_blacklist = effectlimitaionlist
 	data.raw.module["effectivity-module-3"].limitation_message_key = 'lick my balls'
 
 end
