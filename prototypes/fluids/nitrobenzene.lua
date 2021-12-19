5edd0f9b2976dc13bebce0bc4d32ed66e9489259
pyanodon <smylodon@gmail.com>
2021-12-19 13:58:17 -0300

    icons

--- prototypes/fluids/nitrobenzene.lua
+++ prototypes/fluids/nitrobenzene.lua
@@ -18,7 +18,7 @@ FLUID {
     type = "fluid",
     name = "nitrobenzene",
     icon = "__pyhightechgraphics__/graphics/icons/nitrobenzene.png",
-	icon_size = 32,
+	icon_size = 64,
     default_temperature = 10,
     base_color = {r = 0.549, g = 0.384, b = 0.223},
     flow_color = {r = 0.549, g = 0.384, b = 0.223},
