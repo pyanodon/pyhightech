1fc71755270d6be17cd9ee432395176c515824b0
ShadowGlass0 <gkecskem@gmail.com>
2022-04-09 01:11:15 +1000

    Formica fix

--- prototypes/updates/pyalienlife-updates.lua
+++ prototypes/updates/pyalienlife-updates.lua
@@ -7,3 +7,4 @@ TECHNOLOGY("biopolymer"):remove_prereq("biotech")
 
 -- RECIPE CHANGES
 RECIPE("phosphoric-acid3"):change_category("bio-reactor")
+RECIPE("formica"):replace_ingredient('creosote', {type='item', name='sap', amount='5'})
