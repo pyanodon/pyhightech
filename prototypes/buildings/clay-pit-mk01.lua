a44624903ed56c08962db3f7e0f0de4d39f7e96d
ShadowGlass0 <gkecskem@gmail.com>
2022-04-20 17:00:38 +1000

    Early game revamp (#51)

--- prototypes/buildings/clay-pit-mk01.lua
+++ prototypes/buildings/clay-pit-mk01.lua
@@ -13,7 +13,7 @@ RECIPE {
     results = {
         {"clay-pit-mk01", 1}
     }
-}:add_unlock('coal-processing-1')
+}:add_unlock('ceramic')
 
 ITEM {
     type = "item",
--- prototypes/updates/pyalienlife-updates.lua
+++ prototypes/updates/pyalienlife-updates.lua
@@ -7,4 +7,4 @@ TECHNOLOGY("biopolymer"):remove_prereq("biotech")
 
 -- RECIPE CHANGES
 RECIPE("phosphoric-acid3"):change_category("bio-reactor")
-RECIPE("formica"):replace_ingredient('creosote', {type='item', name='sap', amount='5'})
+RECIPE("formica"):replace_ingredient('creosote', {type='item', name='saps', amount='5'})
