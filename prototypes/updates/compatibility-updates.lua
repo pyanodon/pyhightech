9b89e8c07265d8c6756909a7b1fcb15cb1c89d2f
TwentyEighty <49283746+TwentyEighty@users.noreply.github.com>
2022-07-19 09:27:14 -0400

    Remove compat file, moving to pypp (#65)

--- prototypes/updates/compatibility-updates.lua
+++
@@ -1,5 +0,0 @@
-if mods["Teleporters"] then
-    RECIPE("teleporter"):replace_ingredient("advanced-circuit", "electronic-circuit")
-    -- Remove prereqs and let autotech figure it out
-    TECHNOLOGY('teleporter'):remove_pack('chemical-science-pack'):remove_prereq('advanced-electronics')
-end
