f2d569139514ce290bd9fab83e699401606244c7
notnotmelon <zacharypicco@gmail.com>
2024-04-13 15:05:26 -0500

    upscaled icons for phosphorus chain

--- prototypes/fluids/phosphine-gas.lua
+++ prototypes/fluids/phosphine-gas.lua
@@ -2,7 +2,7 @@ FLUID {
     type = "fluid",
     name = "phosphine-gas",
     icon = "__pyhightechgraphics__/graphics/icons/phosphine-gas.png",
-	icon_size = 32,
+	icon_size = 64,
     default_temperature = 15,
     base_color = {r = 0.670, g = 0.011, b = 0.564},
     flow_color = {r = 1, g = 1, b = 1},
