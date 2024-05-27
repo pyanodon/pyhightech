933a6d969d625a085fd07bc93f0657ac9f61dcaf
notnotmelon <zacharypicco@gmail.com>
2024-05-26 20:32:08 -0500

    fix control stage to work with new stdlib

--- control.lua
+++ control.lua
@@ -1,16 +1,20 @@
+require '__pypostprocessing__.lib'
+
 require '__core__/lualib/util'
 
-script.on_event(defines.events.on_cutscene_cancelled, function(event)
-    if not game.active_mods['pyalienlife'] then
-        game.surfaces['nauvis'].create_entity{
-            name = 'crash-site-assembling-machine-1-repaired',
-            position = {-20, -7},
-            force = game.players[event.player_index].force
-        }
-        game.surfaces['nauvis'].create_entity{
-            name = 'crash-site-assembling-machine-1-repaired',
-            position = {-12, 2},
-            force = game.players[event.player_index].force
-        }
-    end
+py.on_event(defines.events.on_cutscene_cancelled, function(event)
+    if game.active_mods['pyalienlife'] then return end
+
+    game.surfaces['nauvis'].create_entity{
+        name = 'crash-site-assembling-machine-1-repaired',
+        position = {-20, -7},
+        force = game.players[event.player_index].force
+    }
+    game.surfaces['nauvis'].create_entity{
+        name = 'crash-site-assembling-machine-1-repaired',
+        position = {-12, 2},
+        force = game.players[event.player_index].force
+    }
 end)
+
+py.finalize_events()
