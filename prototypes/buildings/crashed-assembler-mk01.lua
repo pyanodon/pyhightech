5fcbf5d385309411e525dc73d29e709c34de92fd
przemo1232 <79700515+przemo1232@users.noreply.github.com>
2024-09-15 10:18:13 +0200

    remove excess volume

--- prototypes/buildings/crashed-assembler-mk01.lua
+++ prototypes/buildings/crashed-assembler-mk01.lua
@@ -133,7 +133,6 @@ ENTITY{
             pipe_picture = py.pipe_pictures("assembling-machine-2", nil, {0.0, -0.96}, nil, nil),
             pipe_covers = py.pipe_covers(false, true, true, true),
             volume = 1000,
-            volume = 100,
             pipe_connections = {{flow_direction = "output", position = {-1, 2}, direction = defines.direction.south}}
         },
       off_when_no_fluid_recipe = true
