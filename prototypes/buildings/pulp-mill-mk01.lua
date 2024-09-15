90f2b5042cd3accf811520a9e9e8fa8a40b86da2
kingarthur91 <arthurkoy_durbin@yahoo.com>
2024-09-15 03:19:23 -0400

    car impact -1

--- prototypes/buildings/pulp-mill-mk01.lua
+++ prototypes/buildings/pulp-mill-mk01.lua
@@ -168,7 +168,7 @@ ENTITY {
         },
         off_when_no_fluid_recipe = true
     },
-    vehicle_impact_sound = {filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65},
+    vehicle_impact_sound = {filename = "__base__/sound/car-metal-impact-1.ogg", volume = 0.65},
     working_sound = {
         sound = {filename = "__pyhightechgraphics__/sounds/pulp-mill.ogg", volume = 0.8},
         idle_sound = {filename = "__pyhightechgraphics__/sounds/pulp-mill.ogg", volume = 0.3},
