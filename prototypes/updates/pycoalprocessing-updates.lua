4b77d5b101d6d7a065021579df0f66069457dd28
przemo1232 <79700515+przemo1232@users.noreply.github.com>
2024-09-21 17:00:26 +0200

    update advanced-electronics tech names

--- prototypes/updates/pycoalprocessing-updates.lua
+++ prototypes/updates/pycoalprocessing-updates.lua
@@ -1,8 +1,8 @@
 -- TECH CHANGES
-TECHNOLOGY('mukmoux'):remove_prereq('advanced-electronics')
+TECHNOLOGY('mukmoux'):remove_prereq('advanced-circuit')
 TECHNOLOGY('fluid-separation'):remove_prereq('mukmoux')
 TECHNOLOGY('fuel-production'):remove_prereq('lubricant')
-TECHNOLOGY('energy-2'):remove_prereq('advanced-electronics'):remove_prereq('coal-processing-2')
+TECHNOLOGY('energy-2'):remove_prereq('advanced-circuit'):remove_prereq('coal-processing-2')
 TECHNOLOGY('energy-3'):remove_prereq('fuel-production'):remove_prereq('advanced-material-processing-2')
 TECHNOLOGY('fine-electronics'):remove_prereq('sulfur-processing'):remove_prereq('niobium'):remove_prereq('plastics')
 TECHNOLOGY('fluid-processing-machines-1'):remove_prereq('coal-processing-2'):remove_prereq('py-storage-tanks')
