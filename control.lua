2a732d9494e512fed2d9746ae30e8395d84779ce
notnotmelon <zacharypicco@gmail.com>
2025-01-19 08:16:24 -0600

    Very small refactor

--- control.lua
+++ control.lua
@@ -8,12 +8,12 @@ py.on_event(defines.events.on_cutscene_cancelled, function(event)
     game.surfaces["nauvis"].create_entity {
         name = "crash-site-assembling-machine-1-repaired",
         position = {-20, -7},
-        force = game.players[event.player_index].force
+        force = game.get_player(event.player_index).force
     }
     game.surfaces["nauvis"].create_entity {
         name = "crash-site-assembling-machine-1-repaired",
         position = {-12, 2},
-        force = game.players[event.player_index].force
+        force = game.get_player(event.player_index).force
     }
 end)
 
