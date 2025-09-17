181337a8774e564f3d4d673e535d633204c0ee2e
Lynnatic <lynnatic@posteo.de>
2025-09-17 08:32:20 +0200

    Move FLUID entry prototypes into a single file
    Moving the recipes out of the fluid prototypes
    had the effect that we have now a whole bunch of
    files who each contain only the definition for
    a singular fluid.
    
    This is annoying to navigate, maintain and extend,
    so we move everything into a singular file
    to make everyones life a bit easier.
    
    Does not change mod behaviour.

---
+++ prototypes/fluids/fluids.lua
@@ -0,0 +1,449 @@
+
+FLUID {
+    type = "fluid",
+    name = "acetic-acid",
+    icon = "__pyhightechgraphics__/graphics/icons/acetic-acid.png",
+    icon_size = 32,
+    default_temperature = 10,
+    base_color = {r = 0.588, g = 0.274, b = 0.423},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "b"
+}
+
+FLUID {
+    type = "fluid",
+    name = "acetone",
+    icon = "__pyhightechgraphics__/graphics/icons/acetone.png",
+    icon_size = 32,
+    default_temperature = 15,
+    base_color = {r = 0.0, g = 0.662, b = 0.615},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "f"
+}
+
+FLUID {
+    type = "fluid",
+    name = "acrolein",
+    icon = "__pyhightechgraphics__/graphics/icons/acrolein.png",
+    icon_size = 32,
+    default_temperature = 10,
+    base_color = {r = 0.517, g = 0.250, b = 0},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "b"
+}
+
+FLUID {
+    type = "fluid",
+    name = "ammonia",
+    icon = "__pyhightechgraphics__/graphics/icons/ammonia.png",
+    icon_size = 32,
+    default_temperature = 15,
+    base_color = {r = 0.9, g = 0.9, b = 1},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "a1"
+}
+
+FLUID {
+    type = "fluid",
+    name = "bacteria-2",
+    icon = "__pyhightechgraphics__/graphics/icons/bacteria-2.png",
+    icon_size = 32,
+    default_temperature = 10,
+    base_color = {r = 0.478, g = 0.8, b = 0.784},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "c"
+}
+
+FLUID {
+    type = "fluid",
+    name = "benzene",
+    icon = "__pyhightechgraphics__/graphics/icons/benzene.png",
+    icon_size = 32,
+    default_temperature = 10,
+    base_color = {r = 0.996, g = 0.905, b = 0.862},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "f"
+}
+
+FLUID {
+    type = "fluid",
+    name = "black-liquor",
+    icon = "__pyhightechgraphics__/graphics/icons/black-liquor.png",
+    icon_size = 32,
+    default_temperature = 10,
+    base_color = {r = 0.129, g = 0.0, b = 0.0},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "b"
+}
+
+FLUID {
+    type = "fluid",
+    name = "blood",
+    icon = "__pyhightechgraphics__/graphics/icons/blood.png",
+    icon_size = 32,
+    default_temperature = 10,
+    base_color = {r = 0.854, g = 0.219, b = 0.0},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "f"
+}
+
+FLUID {
+    type = "fluid",
+    name = "carbon-sulfide",
+    icon = "__pyhightechgraphics__/graphics/icons/carbon-sulfide.png",
+    icon_size = 32,
+    default_temperature = 15,
+    base_color = {r = 0.705, g = 0.705, b = 0},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "c"
+}
+
+FLUID {
+    type = "fluid",
+    name = "chloromethane",
+    icon = "__pyhightechgraphics__/graphics/icons/chloromethane.png",
+    icon_size = 32,
+    default_temperature = 15,
+    base_color = {r = 0.321, g = 0.549, b = 0.321},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "c"
+}
+
+FLUID {
+    type = "fluid",
+    name = "cumene",
+    icon = "__pyhightechgraphics__/graphics/icons/cumene.png",
+    icon_size = 32,
+    default_temperature = 10,
+    base_color = {r = 0.00, g = 0.0, b = 0.0},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "c"
+}
+
+FLUID {
+    type = "fluid",
+    name = "cyanic-acid",
+    icon = "__pyhightechgraphics__/graphics/icons/cyanic-acid.png",
+    icon_size = 32,
+    default_temperature = 10,
+    base_color = {r = 0.337, g = 0.454, b = 0.725},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "a2"
+}
+
+FLUID {
+    type = "fluid",
+    name = "dimethyldichlorosilane",
+    icon = "__pyhightechgraphics__/graphics/icons/dimethyldichlorosilane.png",
+    icon_size = 32,
+    default_temperature = 10,
+    base_color = {r = 0.221, g = 0.449, b = 0.221},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "c"
+}
+
+FLUID {
+    type = "fluid",
+    name = "dms",
+    icon = "__pyhightechgraphics__/graphics/icons/dms.png",
+    icon_size = 64,
+    default_temperature = 15,
+    base_color = {r = 0.913, g = 0.878, b = 0.0},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "z"
+}
+
+FLUID {
+    type = "fluid",
+    name = "ech",
+    icon = "__pyhightechgraphics__/graphics/icons/ech.png",
+    icon_size = 32,
+    default_temperature = 10,
+    base_color = {r = 0.02, g = 0.956, b = 0.172},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "c"
+}
+
+FLUID {
+    type = "fluid",
+    name = "etching",
+    icon = "__pyhightechgraphics__/graphics/icons/etching.png",
+    icon_size = 32,
+    default_temperature = 10,
+    base_color = {r = 0, g = 0.286, b = 0},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "c"
+}
+
+FLUID {
+    type = "fluid",
+    name = "ethylene",
+    icon = "__pyhightechgraphics__/graphics/icons/ethylene.png",
+    icon_size = 32,
+    default_temperature = 15,
+    base_color = {r = 0.896, g = 0.805, b = 0.762},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "b"
+}
+
+FLUID {
+    type = "fluid",
+    name = "methanal",
+    icon = "__pyhightechgraphics__/graphics/icons/methanal.png",
+    icon_size = 32,
+    default_temperature = 10,
+    base_color = {r = 0.560, g = 0.219, b = 0.117},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "b"
+}
+
+FLUID {
+    type = "fluid",
+    name = "methane",
+    icon = "__pyhightechgraphics__/graphics/icons/methane.png",
+    icon_size = 32,
+    default_temperature = 15,
+    base_color = {r = 0.458, g = 0.772, b = 0.619},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "a"
+}
+
+FLUID {
+    type = "fluid",
+    name = "msa",
+    icon = "__pyhightechgraphics__/graphics/icons/msa.png",
+    icon_size = 32,
+    default_temperature = 10,
+    base_color = {r = 0.509, g = 0.062, b = 0.0},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "c"
+}
+
+FLUID {
+    type = "fluid",
+    name = "nitrobenzene",
+    icon = "__pyhightechgraphics__/graphics/icons/nitrobenzene.png",
+    icon_size = 64,
+    default_temperature = 10,
+    base_color = {r = 0.549, g = 0.384, b = 0.223},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "b"
+}
+
+FLUID {
+    type = "fluid",
+    name = "phosphine-gas",
+    icon = "__pyhightechgraphics__/graphics/icons/phosphine-gas.png",
+    icon_size = 64,
+    default_temperature = 15,
+    base_color = {r = 0.670, g = 0.011, b = 0.564},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "f"
+}
+
+FLUID {
+    type = "fluid",
+    name = "phosphoric-acid",
+    icon = "__pyhightechgraphics__/graphics/icons/phosphoric-acid.png",
+    icon_size = 64,
+    default_temperature = 10,
+    base_color = {r = 0.780, g = 0.011, b = 0.352},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "c"
+}
+
+FLUID {
+    type = "fluid",
+    name = "phosphorous-acid",
+    icon = "__pyhightechgraphics__/graphics/icons/phosphorous-acid.png",
+    icon_size = 64,
+    default_temperature = 10,
+    base_color = {r = 0.847, g = 0.05, b = 0.98},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "c"
+}
+
+FLUID {
+    type = "fluid",
+    name = "propene",
+    icon = "__pyhightechgraphics__/graphics/icons/propene.png",
+    icon_size = 64,
+    default_temperature = 15,
+    base_color = {r = 0.572, g = 0.435, b = 0.666},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "b"
+}
+
+FLUID {
+    type = "fluid",
+    name = "ralesia-extract",
+    icon = "__pyhightechgraphics__/graphics/icons/ralesia-extract.png",
+    icon_size = 32,
+    default_temperature = 10,
+    base_color = {r = 0.903, g = 0.664, b = 0.18},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "c"
+}
+
+FLUID {
+    type = "fluid",
+    name = "raw-ralesia-extract",
+    icon = "__pyhightechgraphics__/graphics/icons/raw-ralesia-extract.png",
+    icon_size = 32,
+    default_temperature = 10,
+    base_color = {r = 0.803, g = 0.564, b = 0.08},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "b"
+}
+
+FLUID {
+    type = "fluid",
+    name = "ree-slurry",
+    icon = "__pyhightechgraphics__/graphics/icons/rare-earth-slurry.png",
+    icon_size = 32,
+    default_temperature = 10,
+    base_color = {r = 0.027, g = 0.396, b = 0.384},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "c"
+}
+
+FLUID {
+    type = "fluid",
+    name = "ree-solution",
+    icon = "__pyhightechgraphics__/graphics/icons/ree-solution.png",
+    icon_size = 32,
+    default_temperature = 10,
+    base_color = {r = 0.027, g = 0.396, b = 0.384},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "c"
+}
+
+FLUID {
+    type = "fluid",
+    name = "soaked-gel",
+    icon = "__pyhightechgraphics__/graphics/icons/soaked-gel.png",
+    icon_size = 32,
+    default_temperature = 10,
+    base_color = {r = 0.317, g = 0.6, b = 0.764},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-aerogel",
+    order = "c"
+}
+
+FLUID {
+    type = "fluid",
+    name = "tall-oil",
+    icon = "__pyhightechgraphics__/graphics/icons/tall-oil.png",
+    icon_size = 32,
+    default_temperature = 10,
+    base_color = {r = 0.776, g = 0.752, b = 0.466},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "b"
+}
+
+FLUID {
+    type = "fluid",
+    name = "zinc-nanocompound",
+    icon = "__pyhightechgraphics__/graphics/icons/zinc-nanocompound.png",
+    icon_size = 32,
+    default_temperature = 10,
+    base_color = {r = 0.384, g = 0.756, b = 0.4},
+    flow_color = {r = 1, g = 1, b = 1},
+    max_temperature = 100,
+    gas_temperature = 15,
+    subgroup = "py-hightech-fluids",
+    order = "a"
+}
+
