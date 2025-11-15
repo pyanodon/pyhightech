f611013a75c5649ab87f662785b9bb74ad4f1c26
Alyssa Doubrovsky <adoubrovsky@gmail.com>
2025-11-15 15:06:21 -0800

    Cleaning up fusionenergy-update file in rawores
    Added rawores recipes file

--- prototypes/updates/pyindustry-updates.lua
+++ prototypes/updates/pyindustry-updates.lua
@@ -2,6 +2,7 @@
 TECHNOLOGY("railway-mk02"):remove_prereq("niobium")
 TECHNOLOGY("advanced-mining-facilities"):remove_prereq("py-storage-tanks")
 TECHNOLOGY("ht-robotics"):remove_prereq("construction-robotics")
+TECHNOLOGY("radars-mk02"):remove_pack("chemical-science-pack")
 
 -- RECIPE UNLOCKS
 
