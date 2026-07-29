9da150ae188073ec13b83de662319bd6316b3bc6
protocol-1903 <67478786+protocol-1903@users.noreply.github.com>
2026-07-28 19:29:49 -0700

    mark farm buildings as farm buildings (#139)

--- prototypes/updates/pyalienlife-updates.lua
+++ prototypes/updates/pyalienlife-updates.lua
@@ -9,3 +9,9 @@ TECHNOLOGY("biopolymer"):remove_prereq("biotech")
 RECIPE("phosphoric-acid3"):replace_category("chemistry", "bio-reactor")
 RECIPE("formica"):replace_ingredient("creosote", {type = "item", name = "saps", amount = 10})
 RECIPE("urea2"):add_unlock("auog-2")
+
+py.mod_data.farm_buildings["cadaveric-arum"] = {default_module = "cadaveric-arum", domain = "plant"}
+py.mod_data.farm_buildings["antelope-enclosure"] = {default_module = "antelope", domain = "animal"}
+py.mod_data.farm_buildings["kicalk-plantation"] = {default_module = "kicalk", domain = "plant"}
+py.mod_data.farm_buildings["moondrop-greenhouse"] = {default_module = "moondrop", domain = "plant"}
+py.mod_data.farm_buildings["wpu-turd"] = {default_module = "py-sawblade-module-mk01", domain = "plant"}
