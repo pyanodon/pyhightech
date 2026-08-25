48361217065793c5de99ecc03de5404a728fd5ea
protocol_1903 <67478786+protocol-1903@users.noreply.github.com>
2026-08-25 00:33:11 -0700

    Fixed misaligned pipe connection graphics. Resolves https://github.com/pyanodon/pybugreports/issues/1540

--- prototypes/buildings/crashed-assembler-mk01.lua
+++ prototypes/buildings/crashed-assembler-mk01.lua
@@ -87,7 +87,7 @@ ENTITY {
     {
         {
             production_type = "output",
-            pipe_picture = py.pipe_pictures("assembling-machine-2", nil, {0.0, -0.96}, nil, nil),
+            pipe_picture = py.pipe_pictures("assembling-machine-2", nil, {0.0, -0.75}, nil, nil),
             pipe_covers = py.pipe_covers(false, true, true, true),
             volume = 100,
             pipe_connections = {{flow_direction = "output", position = {-1, 1.0}, direction = defines.direction.south}}
