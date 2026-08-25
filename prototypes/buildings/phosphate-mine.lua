5bbf04781c622d811206efefab52cdf63d3ca57e
protocol-1903 <67478786+protocol-1903@users.noreply.github.com>
2026-08-25 00:33:27 -0700

    Fixed misaligned pipe connection graphics. Resolves https://github.com/pyanodon/pybugreports/issues/1540 (#143)

--- prototypes/buildings/phosphate-mine.lua
+++ prototypes/buildings/phosphate-mine.lua
@@ -42,7 +42,7 @@ ENTITY {
     collision_box = {{-4.3, -4.3}, {4.3, 4.3}},
     selection_box = {{-4.5, -4.5}, {4.5, 4.5}},
     input_fluid_box = {
-        pipe_picture = py.pipe_pictures("assembling-machine-2"),
+        pipe_picture = require("__base__.prototypes.entity.assembler-pictures").assembler2pipepictures,
         pipe_covers = _G.pipecoverspictures(),
         volume = 200,
         pipe_connections = {
